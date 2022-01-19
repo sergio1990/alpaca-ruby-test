acb109dca31af8382a897195b0c415dd56b925ee
Sergio Gernyak <sergeg1990@gmail.com>
2022-01-19 23:58:22 +0200

    Add apiversions basic implementation

--- lib/alpaca_api/management_api/api.rb
+++ lib/alpaca_api/management_api/api.rb
@@ -4,7 +4,15 @@ require_relative './apiv1.rb'
 class AlpacaManagementAPI < Grape::API
   mount AlpacaManagementAPIv1 => 'v1'
 
-  get :ping do
-    {text: "pong"}
+  params do
+    optional :ClientID, type: Integer, allow_blank: false
+    optional :ClientTransactionID, type: Integer, allow_blank: false
+  end
+  get :apiversions do
+    {
+      'Value' => [1],
+      'ClientTransactionID' => params[:ClientTransactionID],
+      'ServerTransactionID' => 1
+    }
   end
 end
