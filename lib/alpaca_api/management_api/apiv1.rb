0fcbdd8ecae683e9c840dca5d57b1788328e3eb6
Sergio Gernyak <sergeg1990@gmail.com>
2022-01-20 00:03:22 +0200

    Add v1 description simple implementation

--- lib/alpaca_api/management_api/apiv1.rb
+++ lib/alpaca_api/management_api/apiv1.rb
@@ -1,7 +1,20 @@
 require 'grape'
 
 class AlpacaManagementAPIv1 < Grape::API
-  get :ping do
-    {text: "pong"}
+  params do
+    optional :ClientID, type: Integer, allow_blank: false
+    optional :ClientTransactionID, type: Integer, allow_blank: false
+  end
+  get :description do
+    {
+      'Value' => {
+        'ServerName': 'First Alpaca Device',
+        'Manufacturer': 'Sergey Gernyak',
+        'ManufacturerVersion': 'v1.0.0',
+        'Location': 'Kyiv, UA'
+      },
+      'ClientTransactionID' => params[:ClientTransactionID],
+      'ServerTransactionID' => 1
+    }
   end
 end
